460bea72f403a7f4bed392f5ead5fab5d4f3402a
José Raimundo <j.zmais@gmail.com>
2019-04-25 18:28:49 -0300

    modulação

--- main.py
+++ main.py
@@ -1,41 +1,62 @@
 import math
 import numpy as np
+import random
+
+
+def genereteCoordenates():
+    vec_coordinates = []
+    for i in range(particle_max):
+        coordinates = []
+        coordinates.append(random.uniform(0,  1))
+        coordinates.append(random.uniform(0,  1))
+        coordinates.append(random.uniform(0,  1))
+        vec_coordinates.append(coordinates)
+    return vec_coordinates
+
+
 
 #Return the diffusin coeficient
 def getD():
-    D = 0.01
+    # D ∼ 10^(−6) * m^2 * sec^(−1)
+    D = 0.000001
     return D
-
-
+    
 # Return the relaxation time
 def getTd():
-    Td = 0.01
+    Td = 0.0000011
     return Td
 
-# The step function
-def getU():
-    return t-(vec_x/cd)
-
-
 # Return the inpuse response
-def getG():  
-    division_element = math.sqrt(t**2 - ((vec_x/cd)**2))
-    return getU()*(np.exp(-(t/2*getTd())))*(math.cos(division_element)/division_element)
-     
+def getG(x):  
+    cd = math.sqrt(getD()/getTd())
+    division_element =  math.sqrt(t**2 - ((x/cd)**2))
+    return (t-(x/cd))*(np.exp(-(t/2*getTd())))*(math.cos(division_element)/division_element)
 
 
-
-
-# the wavefront speed
-cd = math.sqrt(getD()/getTd())
+# Variable for configurations
+particle_max = 50
+dimensions = 3
+range_scale = 10
 
 # A multidimensional vector of distance from the transmitter and cd
-vec_x = 0
+vec_x = genereteCoordenates()
 
-vec_time = 0
+# the wavefront speed
 
-t = 0
+t = 2
 
 
-# Test
-print(getG())
+for x in vec_x:
+    # ! Calcular distância Euclidiana de x
+    x_dist = np.linalg.norm(x)
+   # output = np.fft.fft(getG(x[0]))
+
+    
+    output = getG(x_dist)
+    print(output)
+
+
+
+
+
+
---
+++ teste.py
@@ -0,0 +1,30 @@
+# import numpy as np
+# import random
+# particle_max = 50
+# dimensions = 3
+# range_scale = 10
+
+# vec_coordinates = []
+
+
+# for i in range(particle_max):
+#     coordinates = []
+#     coordinates.append(random.uniform(0,  1))
+#     coordinates.append(random.uniform(0,  1))
+#     coordinates.append(random.uniform(0,  1))
+#     vec_coordinates.append(coordinates) 
+# #     print(saida) # [69, 47, 43, 64, 16]
+# print("teste")
+# # t = np.arange(100)
+# # # sp = np.fft.fft(np.sin(t))
+# # # print(np.sin(t))
+
+# # print(np.sin(t))
+# for saida in vec_coordinates:
+#     print(saida)
+
+def teste(x):
+    print(x)
+
+
+teste(3)

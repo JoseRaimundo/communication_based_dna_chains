1318cca7569e22022b3cc72e0e7c38dafcc85435
José Raimundo <j.zmais@gmail.com>
2019-06-11 11:14:17 -0300

    optimizado para dna

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import channel
 import channelChart3D as channel3D
+import delayChart2D as channel2D
 
+# channel3D.chartDelay()
+# channel3D.chartGain()
 
-channel3D.chartDelay()
-channel3D.chartGain()
+channel2D.dnaChart2D()
+
--- versao.py
+++ versao.py
@@ -8,7 +8,7 @@ particle_max = 50e-6
 distancias = np.arange(0,particle_max, particle_max/50)
 
 #tempos
-temp = np.arange(0, 2, 1/1000)
+temp = np.arange(0, 2, 1/10000)
 tempo = []
 for t, i in zip(temp, range(2000)):
   if i%10 == 0:
@@ -20,6 +20,7 @@ def getFrequency(frequecy_max, N):
 
 #Return the diffusin coeficient
 def getD():
+    # return ((5.9e-10)*(5000**-0.588))
     return (10**(-6))
 
 # Return the relaxation time
@@ -36,7 +37,6 @@ def gd():
         if t <= abs(x)/cd:
             continue
         else:
-    
             gd_temp = (#np.exp(-t/(2*getTd()))*(
                         (np.cosh(np.sqrt(t**2 - (abs(x)/cd)**2)))/
                             np.sqrt(t**2 - (abs(x)/cd)**2)
@@ -71,17 +71,36 @@ def getDelay():
     phiB = getPhaseB()
     return -(np.diff(phiB)/np.diff(2*np.pi*getFrequency(frequecy_max, phiB.size)))
 
+constante = 0.00055
+
+# Descomente essa parte para gerar os gráficos 2D
 teste = []
 chave = True
 data = [] 
 for x in distancias:
     if chave:
         chave = False
-        teste = getDelay()
+        teste = getDelay() + constante
     else:
-        teste = teste + getDelay() 
+        teste = teste + getDelay() + constante
     data.append(teste)
+data = np.transpose(np.asarray(data))
+with open('data_delay2d.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+    for i in data:
+        employee_writer.writerow(i)
 
+# Descomente essa parte para gerar os gráficos 3D do delay
+teste = []
+chave = True
+data = [] 
+for x in distancias:
+    if chave:
+        chave = False
+        teste = getDelay()+ constante
+    else:
+        teste = teste + getDelay() + constante
+    data.append(teste)
 
 data = np.transpose(np.asarray(data))
 
@@ -90,6 +109,7 @@ with open('data_delay.csv', mode='w') as employee_file:
     for i in data:
         employee_writer.writerow(i)
 
+# Descomente essa parte para gerar os gráficos 3D do ganho
 
 teste = []
 chave = True
@@ -102,9 +122,7 @@ for x in distancias:
         teste = teste + normalizeGain()
     data.append(teste)
 
-
 data = np.transpose(np.asarray(data))
-
 with open('data_gain.csv', mode='w') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for i in data:
